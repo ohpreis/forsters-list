91d4ba7937d2dd05101563204b2ba0cd140fb1a6
ohpreis <ohpreis@gmail.com>
2016-04-02 21:38:21 -0500

    Haha

--- server/main.js
+++ server/main.js
@@ -11,9 +11,9 @@ if (Meteor.isServer) {
 
       // setup to send email
       smtp = {
-          username: 'postmaster@sandbox82121.mailgun.org',      // eg: server@gentlenode.com
-          password: 'LKs-NyP-Ts4-Xex',                          // eg: 3eeP1gtizk5eziohfervU
-          server:   'smtp.mailgun.org',                         // eg: mail.gandi.net
+          username: ''          //eg: server@gentlenode.com
+          password: '',                          // eg: 3eeP1gtizk5eziohfervU
+          server:   '',                         // eg: mail.gandi.net
           port:     '587'                                       // eg: 587
       };
       process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port + '/';
